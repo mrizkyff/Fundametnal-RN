ae4c26461de67aac89f41bab324e1e8838cb367e
mrizkyff <muhamadrizkyff@gmail.com>
2021-01-10 13:58:43 +0700

    simple styling

--- App.js
+++ App.js
@@ -1,10 +1,48 @@
-import React, { Component } from 'react';
-import {Text, View, Image, TextInput} from 'react-native';
+import React, {Component} from 'react';
+import {Text, View, Image, TextInput, StyleSheet} from 'react-native';
 
 const App = () => {
-  return(
+  return (
+    // <SampleComponent />
     <View>
-      <View style={{width: 80, height:80, backgroundColor: '#0abde3'}}></View>
+      <StylingReactNativeComponent />
+    </View>
+  );
+};
+
+const StylingReactNativeComponent = () => {
+  return (
+    <View>
+      <Text style={styles.text}>Styling Component</Text>
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: '#0abde3',
+          borderWidth: 2,
+          borderColor: 'red',
+          marginTop: 20, 
+          marginLeft: 20, 
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'green',
+    marginLeft: 20,
+    marginTop: 40,
+  }
+})
+
+const SampleComponent = () => {
+  return (
+    <View>
+      <View style={{width: 80, height: 80, backgroundColor: '#0abde3'}}></View>
       <Text>Ian Febrian</Text>
       <Text>Rizky</Text>
       <Home />
@@ -13,31 +51,39 @@ const App = () => {
       <BoxGreen />
       <Profile />
     </View>
-  )
-}
+  );
+};
 
 const Home = () => {
-  return <Text>Halaman Home untuk Ian</Text>
-}
+  return <Text>Halaman Home untuk Ian</Text>;
+};
 
 const Foto = () => {
-  return <Image source={{uri: 'https://placeimg.com/100/100/tech'}} style={{width: 100, height: 100}} />
-}
+  return (
+    <Image
+      source={{uri: 'https://placeimg.com/100/100/tech'}}
+      style={{width: 100, height: 100}}
+    />
+  );
+};
 
-class BoxGreen extends Component{
-  render(){
-    return <Text>Ini Class Component</Text>
+class BoxGreen extends Component {
+  render() {
+    return <Text>Ini Class Component</Text>;
   }
 }
 
-class Profile extends Component{
-  render(){
-    return(
+class Profile extends Component {
+  render() {
+    return (
       <View>
-        <Image source={{uri: 'https://placeimg.com/100/100/animals'}} style={{width: 100, height: 100}} />
+        <Image
+          source={{uri: 'https://placeimg.com/100/100/animals'}}
+          style={{width: 100, height: 100}}
+        />
         <Text>Ini Hewan</Text>
       </View>
-    )
+    );
   }
 }
 
